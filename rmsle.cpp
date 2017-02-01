ed001d5c0d25200815bc9ad6461a1abcff311be6
Sergey <fjdiod@gmail.com>
2017-02-01 03:00:33 +0300

    cleaned code

--- rmsle.cpp
+++ rmsle.cpp
@@ -10,13 +10,13 @@ double rmsle(unordered_map<int, double> *y_true, unordered_map<int, double> *y_p
 
 int main(int argc, char* argv[]) {
     
+    int *ind;
     unordered_map<int, double> table1, table2;
+    
     read_data_csv(string(argv[1]), &table1);
     read_data_csv(string(argv[2]), &table2);
-    cout << "OK: read csv" << endl;
-    int *ind;
     ind = read_indexes(string(argv[3]));
-    cout << "OK: read indexes" << endl;
+    
     cout << rmsle(&table1, &table2, ind, SIZE);
     
     delete ind, table1, table2;
