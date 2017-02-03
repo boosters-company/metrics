68942d66fdad3ef26fe919849e494b29663d905d
Kamikadze4GAME <osskrisha@gmail.com>
2017-02-04 00:11:26 +0200

    Editing prefixes of results
    Finished & OK => "ok:XXXXXX" XXXXXX - result
    Else => "error:EEEEEEEE" EEEEEEE - error

--- f1_score.cpp
+++ f1_score.cpp
@@ -7,33 +7,33 @@ double f1_score(unordered_map<int, double> *y_true, unordered_map<int, double> *
 
 int main(int argc, char* argv[]) {
     std::ios_base::sync_with_stdio (false);
-    
+
     int *ind;
     unordered_map<int, double> table1, table2;
-    
+
     read_data_csv(string(argv[1]), &table1);
     read_data_csv(string(argv[2]), &table2);
     ind = read_indexes(string(argv[3]));
-    
+
     double f1 = f1_score(&table1, &table2, ind, SIZE);
-    
-    cout << "answer: " << f1 << endl;
-    
+
+    cout << "ok:" << f1 << endl;
+
     delete ind, table1, table2;
     return 0;
 }
 
 
 double f1_score(unordered_map<int, double> *y_true, unordered_map<int, double> *y_pred, int *ind, int size) {
-   
+
     double tp, fp, fn, recall, precision;
     int idx;
-    
+
     tp = fp = fn = 0;
-    
+
     for(int i = 0; i < size; i++) {
-        
-        idx = ind[i]; 
+
+        idx = ind[i];
         if((*y_pred).count(idx)) {
             if((*y_pred)[idx] == 1 || (*y_pred)[idx] == 0) {
                 if((*y_true)[idx] == 1) {
@@ -43,21 +43,21 @@ double f1_score(unordered_map<int, double> *y_true, unordered_map<int, double> *
                     if((*y_pred)[idx] == 1) fp++;
                 }
             } else {
-                cout << "Wrong prediction: " << (*y_pred)[idx] << endl;
-                throw "Wrong prediction";
+                cout << "error:Wrong prediction: " << (*y_pred)[idx] << endl;
+                throw "error:Wrong prediction";
             }
         } else {
-                cout << "Index does not exist: " << idx << endl;
-                throw  "Index does not exist";
+                cout << "error:Index does not exist: " << idx << endl;
+                throw  "error:Index does not exist";
         }
     }
-    
+
     recall = tp / (tp + fn);
     if( tp != 0) {
         precision = tp / (tp + fp);
     } else {
         precision = 1;
     }
-    
+
     return 2 * recall * precision / (recall + precision);
 }
--- utils.cpp
+++ utils.cpp
@@ -1,70 +1,70 @@
 #include "utils.h"
 extern int SIZE;
 void read_data_csv(string path, unordered_map<int, double> *data) {
-    
+
     char delim;
     ifstream in;
     in.open(path, ifstream::in);
-    
+
     string line, cell;
     in >> line;
     int ind;
     double val;
-    
+
     if(line.find(',') != string::npos) delim = ',';
     if(line.find(';') != string::npos) delim = ';';
-    
+
     int state = 0;
     while(!in.eof()) {
         in >> line;
-        
-        
+
+
         stringstream line_stream(line);
         while(getline(line_stream, cell, delim)) {
-            
+
             if(state % 2 == 0) {
                 try {
                 ind = stoi(cell);
                 } catch(const invalid_argument& ia) {
-                    
-                    cout << "Invalid argumaent: " << cell << endl ;
-                    throw "Invalid argumaent";
-                    
+
+                    cout << "error:Invalid argumaent: " << cell << endl ;
+                    throw "error:Invalid argumaent";
+
                 } catch(const out_of_range& oor) {
-                    
-                    cout << "Out of range: " << cell << endl;
-                    throw "Out of range";
-                    
+
+                    cout << "error:Out of range: " << cell << endl;
+                    throw "error:Out of range";
+
                 }
             } else {
                 try {
                 val = stod(cell);
                 } catch(const invalid_argument& ia) {
-                    
-                    cout << "Invalid argumaent: " << cell << endl ;
-                    throw "Invalid argumaent";
-                    
+
+                    cout << "error:Invalid argumaent: " << cell << endl ;
+                    throw "error:Invalid argumaent";
+
                 } catch(const out_of_range& oor) {
-                    
-                    cout << "Out of range: " << cell << endl;
-                    throw "Invalid argumaent";
+
+                    cout << "error:Out of range: " << cell << endl;
+                    throw "error:Invalid argumaent";
                     
                 }
                 (*data)[ind] = val;
             }
-            
+
             state++;
-            
+
         }
-        
+
         if(cell.empty() && !line_stream) {
-            
-                cout<<"Empty cell"<<endl;
-                throw "Empty cell";
-                
+
+                cout<<"error:Empty cell"<<endl;
+                throw "error:Empty cell";
+
             }
     }
-    
+
     in.close();
 }
 
@@ -75,14 +75,14 @@ int* read_indexes(string path) {
     int *ind;
     in >> size;
     ind = new int[size];
-    
+
     for(int i = 0; i < size; i++) {
         in >> ind[i];
     }
-    
-    
+
+
     in.close();
-    
+
     SIZE = size;
     return ind;
 }
