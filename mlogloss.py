eb221d2e39586adc9864c8adef062c9d9b1b21e6
konstantin <konstantin.gavrilchik@phystech.edu>
2018-06-13 23:16:59 +0300

    mlogloss

--- mlogloss.py
+++ mlogloss.py
@@ -6,20 +6,22 @@ import sys
 """
 example of usage:
 
-python mlogloss.py ./input/test_target.csv ./input/sample_submission.csv
+python mlogloss.py ./input/test_target.csv ./input/sample_submission.csv ./input/pub_id.csv
 
 """
 
-def mlogloss(true_file="./input/test_target.csv", pred_file="./input/sample_submission.csv"):
+def mlogloss(true_file="./input/test_target.csv", pred_file="./input/sample_submission.csv", id_file="./input/sample_submission.csv"):
     """
     This function get path to true target and submission file
     return: one value - score
     """
-    y_true = pd.read_csv(true_file, header=None)
+    ids = pd.read_csv(id_file, header=None)
+    indicies = ids[ids.iloc[:, 1]==1].index
+    y_true = pd.read_csv(true_file, index_col=0, header=None)
     y_pred = pd.read_csv(pred_file, index_col=0)
-    return log_loss(np.array(y_true), np.array(y_pred))
+    return log_loss(np.array(y_true.iloc[indicies]), np.array(y_pred.iloc[indicies]))
 
 def __main__():
-    print(mlogloss(sys.argv[1], sys.argv[2]))
+    print(mlogloss(sys.argv[1], sys.argv[2], sys.argv[3]))
 
 __main__()
