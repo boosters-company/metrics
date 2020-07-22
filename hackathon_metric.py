9e87ea2676fdd530360d88632e576bb914f1f1c7
boosters <fjdiod@gmail.com>
2020-07-22 19:29:29 +0300

    fix path

--- hackathon_metric.py
+++ hackathon_metric.py
@@ -27,7 +27,7 @@ user['x'] = user['x'].astype(int)
 user['y'] = user['y'].astype(int)
 
 idx = pd.read_csv(sys.argv[3])
-z = np.load('all_data.npy')
+z = np.load('/home/workspace/boosters.pro/backend/metrics/metrics/all_data.npy')
 
 user = user.set_index('x')
 ans = ans.set_index('x')
