bd35515decaefff0bcd5702b3e9fed1a5a3fef3a
boosters <fjdiod@gmail.com>
2020-08-31 22:45:41 +0300

    fix proppant

--- propant_metric.py
+++ propant_metric.py
@@ -9,13 +9,11 @@ sub = pd.read_csv(sys.argv[2])
 idx = set(pd.read_csv(sys.argv[3]).ImageId.values)
 
 # deal with not filled images
-ans_tu = ans[ans.ImageId.isin(idx)]
-sub_tu = sub[sub.ImageId.isin(idx)]
-unused_idx = set(idx) - set(sub_tu.ImageId.unique())
-tmp = pd.DataFrame(unused_idx, columns=['ImageId'])
-tmp['prop_size'] = 0
-sub_tu = pd.concat([sub_tu, tmp])
-
-res, _, _ = contest_metric(ans_tu, sub_tu)
-
-print("ok: {}".format(res))
+ans_tu = ans#[ans.ImageId.isin(idx)]
+sub_tu = sub#[sub.ImageId.isin(idx)]
+if len(sub_tu.ImageId.unique()) != len(idx):
+    print('Error: missing ImageId in submit')
+else:
+    res, chi2, mape = contest_metric(ans_tu, sub_tu)
+    print(chi2, mape)
+    print("ok: {}".format(res))
