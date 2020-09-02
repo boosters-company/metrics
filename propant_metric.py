6ea2309f2a905965156e959a31aaeeb506f9c138
boosters <fjdiod@gmail.com>
2020-09-02 18:17:13 +0300

    fix propant

--- propant_metric.py
+++ propant_metric.py
@@ -9,11 +9,11 @@ sub = pd.read_csv(sys.argv[2])
 idx = set(pd.read_csv(sys.argv[3]).ImageId.values)
 
 # deal with not filled images
-ans_tu = ans#[ans.ImageId.isin(idx)]
-sub_tu = sub#[sub.ImageId.isin(idx)]
+ans_tu = ans[ans.ImageId.isin(idx)]
+sub_tu = sub[sub.ImageId.isin(idx)]
 
 if len(set(idx).intersection(set(sub_tu.ImageId))) != len(idx):
-    print('Error: missing ImageId in submit', len(sub_tu.ImageId.unique()))
+    print('Error: missing ImageId in submit')
 else:
     res, chi2, mape = contest_metric(ans_tu, sub_tu)
     #print(chi2, mape)
