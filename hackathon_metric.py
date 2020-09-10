29fef2c7bb9d17f0f4817ae86059f1fe2a4e5a1e
boosters <fjdiod@gmail.com>
2020-09-10 22:07:20 +0300

    fix hackathon_metric.py

--- hackathon_metric.py
+++ hackathon_metric.py
@@ -4,34 +4,34 @@ import numpy as np
 import sys
 
 
-def score(idx, submission, test, all_data, t):
+def score(idx, submission, test, all_data):
     alpha = 1e-4
     beta = 10
-    submission = submission.loc[idx]
-    test = test.loc[idx]
-    submission['z'] = all_data[submission.y, submission.index.values]
-    test.index = submission.index
-    test['z'] = all_data[test.y, test.index.values]
-    y_err = (test.y - submission.y) ** 2
-    z_err = alpha * (test.z - submission.z) ** 2
-    return np.sqrt(np.sum(y_err + z_err)) + beta*t
+    max_z = abs(all_data).max()
+    pred_y = submission.loc[idx.index.values].y.values
+    y = idx.y.astype(int)
+
+    pred_z = all_data[idx.index.values.astype(int), pred_y.astype(int)]
+    z = all_data[idx.index.values, y]
+    y_err = ((pred_y - y)/3000)** 2
+    z_err = ((pred_z - z)/max_z) ** 2
+    return np.sqrt(np.sum(y_err + z_err))
 
 
 ans = pd.read_csv(sys.argv[1])
 
-user = pd.read_csv(sys.argv[2], header=None)
-time = float(user.iloc[0, 1])
-user = user.iloc[2:]
-user = user.rename({0: 'x', 1: 'y'}, axis=1)
+user = pd.read_csv(sys.argv[2])
 user['x'] = user['x'].astype(int)
 user['y'] = user['y'].astype(int)
 
 idx = pd.read_csv(sys.argv[3])
+idx['x'] = idx['x'].astype(int)
+idx = idx.set_index('x')
 z = np.load('all_data.npy')
 
 user = user.set_index('x')
 ans = ans.set_index('x')
 
-res = score(idx.iloc[:, 0].values, user, ans, z, time)
+res = score(idx, user, ans, z)
 
 print("ok: {}".format(res))
